aba419f3c1321b16a9555298bdf758dfa711c762
Yannsmith <yannpollet83@gmail.com>
2026-02-19 13:05:18 +0100

    feat: add PWA support (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "smokefree-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./assets/style.css",
+  "./assets/script.js",
+  "./manifest.webmanifest",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
